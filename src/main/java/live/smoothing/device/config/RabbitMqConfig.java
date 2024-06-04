8ae3913741e754b3dffac65e880b366433416d2b
doramonz <whs3867@gmail.com>
2024-06-04 14:01:41 +0900

    [Refactor] : mq 수정

--- src/main/java/live/smoothing/device/config/RabbitMqConfig.java
+++ src/main/java/live/smoothing/device/config/RabbitMqConfig.java
@@ -1,6 +1,8 @@
 package live.smoothing.device.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
@@ -14,6 +16,7 @@ import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class RabbitMqConfig {
@@ -45,13 +48,17 @@ public class RabbitMqConfig {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,true);
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 
     @Bean
@@ -66,44 +73,44 @@ public class RabbitMqConfig {
     public SimpleRabbitListenerContainerFactory jsonContainerFactory(ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jackson2JsonMessageConverter());
+        factory.setMessageConverter(jackson2JsonMessageConverter(null));
         return factory;
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter(null));
         return rabbitTemplate;
     }
 
-    @Bean
-    public DirectExchange brokerDirectExchange() {
-        return new DirectExchange("error-direct-exchange");
-    }
-
-    @Bean
-    public Queue addBrokerErrorQueue() {
-        return new Queue("add-brokerError-queue");
-    }
-
-    @Bean
-    public Queue addSensorErrorQueue() {
-        return new Queue("add-sensorError-queue");
-    }
-
-    @Bean
-    public Binding addBrokerBinding() {
-        return BindingBuilder.bind(addBrokerErrorQueue())
-                .to(brokerDirectExchange())
-                .with("add-brokerError");
-    }
-
-    @Bean
-    public Binding addSensorErrorBinding() {
-        return BindingBuilder.bind(addSensorErrorQueue())
-                .to(brokerDirectExchange())
-                .with("add-sensorError");
-    }
+//    @Bean
+//    public DirectExchange brokerDirectExchange() {
+//        return new DirectExchange("error-direct-exchange");
+//    }
+//
+//    @Bean
+//    public Queue addBrokerErrorQueue() {
+//        return new Queue("add-brokerError-queue");
+//    }
+//
+//    @Bean
+//    public Queue addSensorErrorQueue() {
+//        return new Queue("add-sensorError-queue");
+//    }
+//
+//    @Bean
+//    public Binding addBrokerBinding() {
+//        return BindingBuilder.bind(addBrokerErrorQueue())
+//                .to(brokerDirectExchange())
+//                .with("add-brokerError");
+//    }
+//
+//    @Bean
+//    public Binding addSensorErrorBinding() {
+//        return BindingBuilder.bind(addSensorErrorQueue())
+//                .to(brokerDirectExchange())
+//                .with("add-sensorError");
+//    }
 
 }
